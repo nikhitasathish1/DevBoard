2e90a7453442527da085eab41178d6429400bb55
Nikhita Sathish <nikhita.sathish01@gmail.com>
2025-06-08 11:49:09 +0530

    frontend + websocket updates

--- backend/core/consumers.py
+++ backend/core/consumers.py
@@ -3,10 +3,9 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class BoardConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.board_id = self.scope["url_route"]["kwargs"]["board_id"]
+        self.board_id = self.scope['url_route']['kwargs']['board_id']
         self.room_group_name = f"board_{self.board_id}"
 
-        # Join board group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -15,70 +14,72 @@ class BoardConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave board group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         data = json.loads(text_data)
-        message = data["message"]
+        event_type = data.get("type")
 
-        # Broadcast to group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "board_message",
-                "message": message
-            }
-        )
+        # Debugging log
+        print(f"Received event: {data}")
 
-    # Receive message from group
-    async def board_message(self, event):
-        await self.send(text_data=json.dumps({
-            "message": event["message"]
-        }))
+        if event_type and event_type.startswith(("card.", "column.")):
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "broadcast_event",
+                    "event": data
+                }
+            )
 
+    async def broadcast_event(self, event):
+        await self.send(text_data=json.dumps(event["event"]))
 
-# from channels.generic.websocket import AsyncWebsocketConsumer
 # import json
+# from channels.generic.websocket import AsyncWebsocketConsumer
 
 # class BoardConsumer(AsyncWebsocketConsumer):
 #     async def connect(self):
-#         self.board_id = self.scope['url_route']['kwargs']['board_id']
-#         self.board_group_name = f'board_{self.board_id}'
+#         self.board_id = self.scope["url_route"]["kwargs"]["board_id"]
+#         self.room_group_name = f"board_{self.board_id}"
 
-#         # Join room group
+#         # Join board group
 #         await self.channel_layer.group_add(
-#             self.board_group_name,
+#             self.room_group_name,
 #             self.channel_name
 #         )
 
 #         await self.accept()
 
 #     async def disconnect(self, close_code):
+#         # Leave board group
 #         await self.channel_layer.group_discard(
-#             self.board_group_name,
+#             self.room_group_name,
 #             self.channel_name
 #         )
 
+#     # Receive message from WebSocket
 #     async def receive(self, text_data):
 #         data = json.loads(text_data)
-#         message = data['message']
+#         message = data["message"]
 
 #         # Broadcast to group
 #         await self.channel_layer.group_send(
-#             self.board_group_name,
+#             self.room_group_name,
 #             {
-#                 'type': 'chat_message',
-#                 'message': message
+#                 "type": "board_message",
+#                 "message": message
 #             }
 #         )
 
-#     async def chat_message(self, event):
+#     # Receive message from group
+#     async def board_message(self, event):
 #         await self.send(text_data=json.dumps({
-#             'message': event['message']
+#             "message": event["message"]
 #         }))
+
+
 
